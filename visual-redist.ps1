72b176465057d86b476401bc8336d81d39aee371
Samantha <19739806+UltimateNova1203@users.noreply.github.com>
2021-09-06 12:07:17 -0700

    Refactor 2.1a

--- visual-redist.ps1
+++ visual-redist.ps1
@@ -24,42 +24,42 @@ New-Item -Path C:\visual-redist -ItemType Directory
 if ($OSArch -eq "X86" -Or $OSarch -eq "AMD64") {
 	#VC2005-x86
 	Write-Output "Downloading Visual C++ 2005 x86 Redistributable..."
-	Invoke-WebRequest -Uri "https://raw.githubusercontent.com/UltimateNova1203/visual-redist/master/vc2005-x86.exe" -OutFile C:\visual-redist\vc2005-x86.exe
+	Invoke-WebRequest -Uri "https://raw.githubusercontent.com/UltimateNova1203/visual-redist/master/vc/vc2005-x86.exe" -OutFile C:\visual-redist\vc2005-x86.exe
 	Write-Output "Installing Visual C++ 2005 x86 Redistributable..."
 	Start-Process C:\visual-redist\vc2005-x86.exe -ArgumentList "/q" -wait
 	Write-Output "Visual C++ 2005 x86 Redistributable Installed!"
 
 	#VC2008-x86
 	Write-Output "Downloading Visual C++ 2008 x86 Redistributable..."
-	Invoke-WebRequest -Uri "https://raw.githubusercontent.com/UltimateNova1203/visual-redist/master/vc2008-x86.exe" -OutFile C:\visual-redist\vc2008-x86.exe
+	Invoke-WebRequest -Uri "https://raw.githubusercontent.com/UltimateNova1203/visual-redist/master/vc/vc2008-x86.exe" -OutFile C:\visual-redist\vc2008-x86.exe
 	Write-Output "Installing Visual C++ 2008 x86 Redistributable..."
 	Start-Process C:\visual-redist\vc2008-x86.exe -ArgumentList "/Q" -wait
 	Write-Output "Visual C++ 2008 x86 Redistributable Installed!"
 
 	#VC2010-x86
 	Write-Output "Downloading Visual C++ 2010 x86 Redistributable..."
-	Invoke-WebRequest -Uri "https://raw.githubusercontent.com/UltimateNova1203/visual-redist/master/vc2010-x86.exe" -OutFile C:\visual-redist\vc2010-x86.exe
+	Invoke-WebRequest -Uri "https://raw.githubusercontent.com/UltimateNova1203/visual-redist/master/vc/vc2010-x86.exe" -OutFile C:\visual-redist\vc2010-x86.exe
 	Write-Output "Installing Visual C++ 2010 x86 Redistributable..."
 	Start-Process C:\visual-redist\vc2010-x86.exe -ArgumentList "/q /norestart" -wait
 	Write-Output "Visual C++ 2010 x86 Redistributable Installed!"
 
 	#VC2012-x86
 	Write-Output "Downloading Visual C++ 2012 x86 Redistributable..."
-	Invoke-WebRequest -Uri "https://raw.githubusercontent.com/UltimateNova1203/visual-redist/master/vc2012-x86.exe" -OutFile C:\visual-redist\vc2012-x86.exe
+	Invoke-WebRequest -Uri "https://raw.githubusercontent.com/UltimateNova1203/visual-redist/master/vc/vc2012-x86.exe" -OutFile C:\visual-redist\vc2012-x86.exe
 	Write-Output "Installing Visual C++ 2012 x86 Redistributable..."
 	Start-Process C:\visual-redist\vc2012-x86.exe -ArgumentList "/install /quiet /norestart" -wait
 	Write-Output "Visual C++ 2012 x86 Redistributable Installed!"
 
 	#VC2013-x86
 	Write-Output "Downloading Visual C++ 2013 x86 Redistributable..."
-	Invoke-WebRequest -Uri "https://raw.githubusercontent.com/UltimateNova1203/visual-redist/master/vc2013-x86.exe" -OutFile C:\visual-redist\vc2013-x86.exe
+	Invoke-WebRequest -Uri "https://raw.githubusercontent.com/UltimateNova1203/visual-redist/master/vc/vc2013-x86.exe" -OutFile C:\visual-redist\vc2013-x86.exe
 	Write-Output "Installing Visual C++ 2013 x86 Redistributable..."
 	Start-Process C:\visual-redist\vc2013-x86.exe -ArgumentList "/install /quiet /norestart" -wait
 	Write-Output "Visual C++ 2013 x86 Redistributable Installed!"
 
 	#VC2019-x86
 	Write-Output "Downloading Visual C++ 2019 x86 Redistributable..."
-	Invoke-WebRequest -Uri "https://raw.githubusercontent.com/UltimateNova1203/visual-redist/master/vc2015=9-x86.exe" -OutFile C:\visual-redist\vc2019-x86.exe
+	Invoke-WebRequest -Uri "https://raw.githubusercontent.com/UltimateNova1203/visual-redist/master/vc/vc2015=9-x86.exe" -OutFile C:\visual-redist\vc2019-x86.exe
 	Write-Output "Installing Visual C++ 2019 x86 Redistributable..."
 	Start-Process C:\visual-redist\vc2019-x86.exe -ArgumentList "/install /quiet /norestart" -wait
 	Write-Output "Visual C++ 2019 x86 Redistributable Installed!"
@@ -69,42 +69,42 @@ if ($OSArch -eq "X86" -Or $OSarch -eq "AMD64") {
 if ($OSArch -eq "AMD64") {
 	#VC2005-x64
 	Write-Output "Downloading Visual C++ 2005 x64 Redistributable..."
-	Invoke-WebRequest -Uri "https://raw.githubusercontent.com/UltimateNova1203/visual-redist/master/vc2005-x64.exe" -OutFile C:\visual-redist\vc2005-x64.exe
+	Invoke-WebRequest -Uri "https://raw.githubusercontent.com/UltimateNova1203/visual-redist/master/vc/vc2005-x64.exe" -OutFile C:\visual-redist\vc2005-x64.exe
 	Write-Output "Installing Visual C++ 2005 x64 Redistributable..."
 	Start-Process C:\visual-redist\vc2005-x64.exe -ArgumentList "/q" -wait
 	Write-Output "Visual C++ 2005 x64 Redistributable Installed!"
 
 	#VC2008-x64
 	Write-Output "Downloading Visual C++ 2008 x64 Redistributable..."
-	Invoke-WebRequest -Uri "https://raw.githubusercontent.com/UltimateNova1203/visual-redist/master/vc2008-x64.exe" -OutFile C:\visual-redist\vc2008-x64.exe
+	Invoke-WebRequest -Uri "https://raw.githubusercontent.com/UltimateNova1203/visual-redist/master/vc/vc2008-x64.exe" -OutFile C:\visual-redist\vc2008-x64.exe
 	Write-Output "Installing Visual C++ 2008 x64 Redistributable..."
 	Start-Process C:\visual-redist\vc2008-x64.exe -ArgumentList "/Q" -Wait
 	Write-Output "Visual C++ 2008 x64 Redistributable Installed!"
 
 	#VC2010-x64
 	Write-Output "Downloading Visual C++ 2010 x64 Redistributable..."
-	Invoke-WebRequest -Uri "https://raw.githubusercontent.com/UltimateNova1203/visual-redist/master/vc2010-x64.exe" -OutFile C:\visual-redist\vc2010-x64.exe
+	Invoke-WebRequest -Uri "https://raw.githubusercontent.com/UltimateNova1203/visual-redist/master/vc/vc2010-x64.exe" -OutFile C:\visual-redist\vc2010-x64.exe
 	Write-Output "Installing Visual C++ 2010 x64 Redistributable..."
 	Start-Process C:\visual-redist\vc2010-x64.exe -ArgumentList "/q /norestart" -Wait
 	Write-Output "Visual C++ 2010 x64 Redistributable Installed!"
 
 	#VC2012-x64
 	Write-Output "Downloading Visual C++ 2012 x64 Redistributable..."
-	Invoke-WebRequest -Uri "https://raw.githubusercontent.com/UltimateNova1203/visual-redist/master/vc2012-x64.exe" -OutFile C:\visual-redist\vc2012-x64.exe
+	Invoke-WebRequest -Uri "https://raw.githubusercontent.com/UltimateNova1203/visual-redist/master/vc/vc2012-x64.exe" -OutFile C:\visual-redist\vc2012-x64.exe
 	Write-Output "Installing Visual C++ 2012 x64 Redistributable..."
 	Start-Process C:\visual-redist\vc2012-x64.exe -ArgumentList "/install /quiet /norestart" -wait
 	Write-Output "Visual C++ 2012 x64 Redistributable Installed!"
 
 	#VC2013-x64
 	Write-Output "Downloading Visual C++ 2013 x64 Redistributable..."
-	Invoke-WebRequest -Uri "https://raw.githubusercontent.com/UltimateNova1203/visual-redist/master/vc2013-x64.exe" -OutFile C:\visual-redist\vc2013-x64.exe
+	Invoke-WebRequest -Uri "https://raw.githubusercontent.com/UltimateNova1203/visual-redist/master/vc/vc2013-x64.exe" -OutFile C:\visual-redist\vc2013-x64.exe
 	Write-Output "Installing Visual C++ 2013 x64 Redistributable..."
 	Start-Process C:\visual-redist\vc2013-x64.exe -ArgumentList "/install /quiet /norestart" -wait
 	Write-Output "Visual C++ 2013 x64 Redistributable Installed!"
 
 	#VC2019-x64
 	Write-Output "Downloading Visual C++ 2019 x64 Redistributable..."
-	Invoke-WebRequest -Uri "https://raw.githubusercontent.com/UltimateNova1203/visual-redist/master/vc2019-x64.exe" -OutFile C:\visual-redist\vc2019-x64.exe
+	Invoke-WebRequest -Uri "https://raw.githubusercontent.com/UltimateNova1203/visual-redist/master/vc/vc2019-x64.exe" -OutFile C:\visual-redist\vc2019-x64.exe
 	Write-Output "Installing Visual C++ 2019 x64 Redistributable..."
 	Start-Process C:\visual-redist\vc2019-x64.exe -ArgumentList "/install /quiet /norestart" -wait
 	Write-Output "Visual C++ 2019 x64 Redistributable Installed!"
@@ -113,21 +113,21 @@ if ($OSArch -eq "AMD64") {
 if ($OSArch -eq "ARM64") {
 	#VC2012-x64
 	Write-Output "Downloading Visual C++ 2012 ARM64 Redistributable..."
-	Invoke-WebRequest -Uri "https://raw.githubusercontent.com/UltimateNova1203/visual-redist/master/vc2012-arm64.exe" -OutFile C:\visual-redist\vc2012-arm64.exe
+	Invoke-WebRequest -Uri "https://raw.githubusercontent.com/UltimateNova1203/visual-redist/master/vc/vc2012-arm64.exe" -OutFile C:\visual-redist\vc2012-arm64.exe
 	Write-Output "Installing Visual C++ 2012 ARM64 Redistributable..."
 	Start-Process C:\visual-redist\vc2012-arm64.exe -ArgumentList "/install /quiet /norestart" -wait
 	Write-Output "Visual C++ 2012 ARM64 Redistributable Installed!"
 
 	#VC2013-x64
 	Write-Output "Downloading Visual C++ 2013 ARM64 Redistributable..."
-	Invoke-WebRequest -Uri "https://raw.githubusercontent.com/UltimateNova1203/visual-redist/master/vc2013-arm64.exe" -OutFile C:\visual-redist\vc2013-arm64.exe
+	Invoke-WebRequest -Uri "https://raw.githubusercontent.com/UltimateNova1203/visual-redist/master/vc/vc2013-arm64.exe" -OutFile C:\visual-redist\vc2013-arm64.exe
 	Write-Output "Installing Visual C++ 2013 ARM64 Redistributable..."
 	Start-Process C:\visual-redist\vc2013-arm64.exe -ArgumentList "/install /quiet /norestart" -wait
 	Write-Output "Visual C++ 2013 ARM64 Redistributable Installed!"
 
 	#VC2019-x64
 	Write-Output "Downloading Visual C++ 2019 ARM64 Redistributable..."
-	Invoke-WebRequest -Uri "https://raw.githubusercontent.com/UltimateNova1203/visual-redist/master/vc2019-arm64.exe" -OutFile C:\visual-redist\vc2019-arm64.exe
+	Invoke-WebRequest -Uri "https://raw.githubusercontent.com/UltimateNova1203/visual-redist/master/vc/vc2019-arm64.exe" -OutFile C:\visual-redist\vc2019-arm64.exe
 	Write-Output "Installing Visual C++ 2019 ARM64 Redistributable..."
 	Start-Process C:\visual-redist\vc2019-arm64.exe -ArgumentList "/install /quiet /norestart" -wait
 	Write-Output "Visual C++ 2019 ARM64 Redistributable Installed!"
@@ -136,14 +136,14 @@ if ($OSArch -eq "ARM64") {
 if ($OSArch -eq "IA64") {
 	#VC2008-IA64
 	Write-Output "Downloading Visual C++ 2008 IA64 Redistributable..."
-	Invoke-WebRequest -Uri "https://raw.githubusercontent.com/UltimateNova1203/visual-redist/master/vc2008-ia64.exe" -OutFile C:\visual-redist\vc2008-ia64.exe
+	Invoke-WebRequest -Uri "https://raw.githubusercontent.com/UltimateNova1203/visual-redist/master/vc/vc2008-ia64.exe" -OutFile C:\visual-redist\vc2008-ia64.exe
 	Write-Output "Installing Visual C++ 2008 IA64 Redistributable..."
 	Start-Process C:\visual-redist\vc2008-ia64.exe -ArgumentList "/Q" -Wait
 	Write-Output "Visual C++ 2008 IA64 Redistributable Installed!"
 
 	#VC2010-IA64
 	Write-Output "Downloading Visual C++ 2010 IA64 Redistributable..."
-	Invoke-WebRequest -Uri "https://raw.githubusercontent.com/UltimateNova1203/visual-redist/master/vc2010-ia64.exe" -OutFile C:\visual-redist\vc2010-ia64.exe
+	Invoke-WebRequest -Uri "https://raw.githubusercontent.com/UltimateNova1203/visual-redist/master/vc/vc2010-ia64.exe" -OutFile C:\visual-redist\vc2010-ia64.exe
 	Write-Output "Installing Visual C++ 2010 IA64 Redistributable..."
 	Start-Process C:\visual-redist\vc2010-ia64.exe -ArgumentList "/q /norestart" -Wait
 	Write-Output "Visual C++ 2010 IA64 Redistributable Installed!"
